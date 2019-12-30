b495e605123d40402a57f19a37410010eb987139
Justin Zollars <fogonthedowns@justins-mbp.attlocal.net>
2019-12-30 10:33:04 -0800

    Context Clues

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PlayerState {
+    walk,
+    attack,
+    interact,
+    stagger,
+    idle
+}
+
+public class PlayerMovement : MonoBehaviour {
+
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+    public FloatValue currentHealth;
+    public Signal_Event playerHealthSignal;
+    public VectorValue startingPosition;
+
+    // Start is called before the first frame update
+    void Start(){
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
+        transform.position = startingPosition.initialValue;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetButtonDown("attack") && currentState != PlayerState.attack
+            && currentState != PlayerState.stagger) {
+            StartCoroutine(AttackCo());
+        }
+
+        else if (currentState == PlayerState.walk ||
+                 currentState == PlayerState.idle)
+        {
+            UpdateAnimationAndMove();
+        }
+
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking",true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+        myRigidBody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+
+    public void Knock(float KnockTime, float damage)
+    {
+        currentHealth.RuntimeValue -= damage;
+        playerHealthSignal.Raise();
+        if (currentHealth.RuntimeValue > 0)
+        {
+            // Raise() could be a sound could be a lot of stuff
+           // playerHealthSignal.Raise();
+            StartCoroutine(KnockCo(KnockTime));
+
+        } else
+        {
+            // Gameover could be signal
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator KnockCo(float knockTime)
+    {
+        if (myRigidBody != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            myRigidBody.velocity = Vector2.zero;
+            currentState = PlayerState.idle;
+            myRigidBody.velocity = Vector2.zero;
+        }
+    }
+}
